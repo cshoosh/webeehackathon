80d4f1423c1da4bfc2b55c1c2cb5d9d40dd31c64
Syed Shahnawaz Ali <syed.shahnawaz@royalcyber.com>
2022-11-19 11:05:38 +0500

    Added react navigation with Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
+import AppNavigation from "./navigations/AppNavigation";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}></PersistGate>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaProvider>
+            <AppNavigation />
+          </SafeAreaProvider>
+        </PersistGate>
       </Provider>
       <StatusBar style="auto" />
     </View>
